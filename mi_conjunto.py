de5b1cce9636e4679789edc50caf82d331db0620
Daniel-Villar <a.22308051281330@cbtis128.edu.mx>
2024-09-11 07:49:52 -0600

    Add files via upload

---
+++ mi_conjunto.py
@@ -0,0 +1,7 @@
+print("Ejemplo de conjuntos")
+tipodeportes={"futbol","Tenis","Basquetball"}
+print(tipodeportes)
+print("Lista de elementos del conjunto tipo de deportes loop for")
+print(tipodeportes)
+for villlar  in tipodeportes: 
+    print(villlar)
---
+++ mi_diccionario.py
@@ -0,0 +1,9 @@
+print("Ejemplo de diccionario")
+dicc = {
+    "Grupo": "Stray kids",
+    "Lider": "Bangchan",
+    "fecha de creacion": 2016 
+} 
+print(dicc)
+for x, y in dicc.items():
+  print(x, y)
